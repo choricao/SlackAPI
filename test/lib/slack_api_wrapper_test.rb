7ad45e3ce8836ad0383c88dd5686283ac51eb120
Zheng Cao <chori84@gmail.com>
2018-05-02 10:46:38 -0700

    Test negative case for SlackApiWrapper

--- test/lib/slack_api_wrapper_test.rb
+++ test/lib/slack_api_wrapper_test.rb
@@ -8,9 +8,17 @@ describe SlackApiWrapper do
 
       response = SlackApiWrapper.send_message("CAGCPB64A", message)
 
-      # or test response.must_equal true
+      # or test response.must_equal true if method return response.success?
       response["ok"].must_equal true
       response["message"]["text"].must_equal message
+    end
+  end
+
+  it "Can't send message to fake channel" do
+    VCR.use_cassette("channels") do
+      response = SlackApiWrapper.send_message("this-channel-does-not-exist", "test message")
+      response["ok"].must_equal false
+      response["error"].wont_be_nil
     end
   end
 
