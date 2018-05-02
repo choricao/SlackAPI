6d2801c2e9039388d6994226bb18d105f5f483e8
Zheng Cao <chori84@gmail.com>
2018-05-02 09:28:25 -0700

    Fix Minitest bug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,11 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
 
+# Needed because SlackAPI project was upgraded in a unique way, this shouldn't need to be done for any new Rails project
+# https://github.com/rails/rails/issues/31324
+if ActionPack::VERSION::STRING >= "5.2.0"
+  Minitest::Rails::TestUnit = Rails::TestUnit
+end
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
